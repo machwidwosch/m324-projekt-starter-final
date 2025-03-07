590ed65097f1b94ba316f836b3773ef5a47b4a48
switzerchees <patrickmichel96@yahoo.de>
2025-03-07 15:59:35 +0100

    Update eslint config only lint .ts files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  { ignores: ['*.js', 'client', 'build'] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
